a9fcd86220b767f4892300ee0329d3cc435a37dc
garlic0x1 <garlic1@protonmail.com>
2023-12-07 03:27:54 -0600

    restructure utils

--- vm/src/util/bits.c
+++ vm/src/util/bits.c
@@ -2,6 +2,19 @@
 #include "util.h"
 #include <stdint.h>
 
+uint16_t swap16(uint16_t x)
+{
+	return (x << 8) | (x >> 8);
+}
+
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+	if ((x >> (bit_count - 1)) & 1) {
+		x |= (0xFFFF << bit_count);
+	}
+	return x;
+}
+
 uint16_t extract_bits(uint16_t n, uint8_t a, uint8_t b)
 {
 	// removing check will make faster...
--- vm/src/util/util.c
+++ vm/src/util/util.c
@@ -1,21 +1,9 @@
 #include "util.h"
+#include "bits.h"
 #include "io.h"
 
 #include <signal.h>
 #include <stdlib.h>
-
-uint16_t sign_extend(uint16_t x, int bit_count)
-{
-	if ((x >> (bit_count - 1)) & 1) {
-		x |= (0xFFFF << bit_count);
-	}
-	return x;
-}
-
-uint16_t swap16(uint16_t x)
-{
-	return (x << 8) | (x >> 8);
-}
 
 void update_flags(struct vm_state *state, uint16_t r)
 {
